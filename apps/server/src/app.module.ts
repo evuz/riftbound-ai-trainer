da49b6caf959ce0a3bc0d79e3458f5b98b07fe96
Jesus Gómez <jesgombel@outlook.es>
2026-05-03 11:10:53 +0200

    refactor: move health module to presentation/api

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { HealthModule } from './health/health.module';
+import { HealthModule } from './presentation/api/health/health.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), HealthModule],
